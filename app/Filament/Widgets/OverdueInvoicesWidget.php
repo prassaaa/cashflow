eeb7da9c990c165c0f9e5d28e78256102d7b2964
Prass <pras.ari69@gmail.com>
2026-01-16 11:39:58 +0700

    refactor: Add visibility check for OverdueInvoicesWidget based on user roles

--- app/Filament/Widgets/OverdueInvoicesWidget.php
+++ app/Filament/Widgets/OverdueInvoicesWidget.php
@@ -6,6 +6,7 @@ use App\Models\Invoice;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget;
+use Illuminate\Support\Facades\Auth;
 
 class OverdueInvoicesWidget extends TableWidget
 {
@@ -14,6 +15,13 @@ class OverdueInvoicesWidget extends TableWidget
     protected int|string|array $columnSpan = 'full';
 
     protected static ?string $heading = 'Invoice Jatuh Tempo';
+
+    public static function canView(): bool
+    {
+        // super_admin, marketing, and accounting can see overdue invoices
+        $user = Auth::user();
+        return $user?->hasAnyRole(['super_admin', 'marketing', 'accounting']);
+    }
 
     public function table(Table $table): Table
     {
