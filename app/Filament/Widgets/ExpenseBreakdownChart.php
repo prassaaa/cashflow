baf8438932e4f87008221d4d378bedace6e3f102
Prass <pras.ari69@gmail.com>
2026-01-18 17:08:02 +0700

    feat: Add columnSpan property to ExpenseBreakdownChart for layout customization

--- app/Filament/Widgets/ExpenseBreakdownChart.php
+++ app/Filament/Widgets/ExpenseBreakdownChart.php
@@ -16,6 +16,8 @@ class ExpenseBreakdownChart extends ChartWidget
 
     protected static ?int $sort = 5;
 
+    protected int|string|array $columnSpan = 1;
+
     public static function canView(): bool
     {
         // Only super_admin and accounting can see expense breakdown
