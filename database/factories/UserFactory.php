a0cb5f05b95333808a1f46d82be9ac6a75ba1113
Prass <pras.ari69@gmail.com>
2026-01-21 20:57:16 +0700

    fix: Refactor UserFactory to use the fake() helper for name and email generation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = \Faker\Factory::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
