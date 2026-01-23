b41135b35dd3b5ec27df1577b589316633cb6fef
Prass <pras.ari69@gmail.com>
2026-01-23 23:23:13 +0700

    feat: Add EditHrdAttendance and ListHrdAttendances pages for HRD attendance management

---
+++ app/Filament/Resources/HrdAttendances/Pages/EditHrdAttendance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\HrdAttendances\Pages;
+
+use App\Filament\Resources\HrdAttendances\HrdAttendanceResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditHrdAttendance extends EditRecord
+{
+    protected static string $resource = HrdAttendanceResource::class;
+}
---
+++ app/Filament/Resources/HrdAttendances/Pages/ListHrdAttendances.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\HrdAttendances\Pages;
+
+use App\Filament\Resources\HrdAttendances\HrdAttendanceResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListHrdAttendances extends ListRecords
+{
+    protected static string $resource = HrdAttendanceResource::class;
+}
